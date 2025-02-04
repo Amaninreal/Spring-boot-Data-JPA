788ca45ca31079dfa2dbcfbafda2fa03139c1b42
Amaninreal <amaninreallife03@gmail.com>
2025-02-04 23:50:42 +0530

    update crud feature using spring-data-rest

--- src/main/java/com/rest/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/rest/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,10 @@ package com.rest.springboot.cruddemo.dao;
 
 import com.rest.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> { // Entity type -> Employee, Primary key -> Integer
 
 }
